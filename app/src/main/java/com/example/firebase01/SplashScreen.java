bfdf5f0d9d7bb0d6cc850837949117f7d281719d
hn004 <harshalnikam123>
2020-12-05 14:39:12 +0530

    ADDED SplashScreen

---
+++ app/src/main/java/com/example/firebase01/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.example.firebase01;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+}
